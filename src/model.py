fe79983bac8dd6e376c7081ebdc835cac6fd9ab8
Robert Mitru <robert.mitru.business@gmail.com>
2023-10-08 23:44:51 +0300

    Created simple regression models and optimized the code structure

---
+++ src/model.py
@@ -0,0 +1,40 @@
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+def run_model(X, y, pipeline, grid=None, scorer=None, rs=42):
+    """
+    """
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                        test_size=0.3,
+                                                        # stratify=y,
+                                                        random_state=rs)
+
+    # gscv = GridSearchCV(estimator=pipeline,
+    #                     param_grid=grid,
+    #                     scoring=scorer,
+    #                     n_jobs=-1,
+    #                     cv=5)
+
+    # model = gscv.fit(X_train, y_train)
+
+    model = pipeline.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+
+    results = {
+        # 'fitted_gscv': model,
+        # 'best_model': model.best_estimator_.named_steps['model'],
+        # 'tuned_params': pd.Series(model.best_params_),
+        # 'accuracy': accuracy_score(y_test, y_pred),
+        # 'precision': precision_score(y_test, y_pred),
+        # 'recall': recall_score(y_test, y_pred),
+        # 'f1': f1_score(y_test, y_pred),
+        # 'y_true': y_test,
+        # 'y_predicted': pd.Series(data=y_pred, index=X_test.index)
+
+        'model': model,
+        'train_score': model.score(X_train, y_train),
+        'test_score': model.score(X_test, y_test)
+    }
+
+    return results
